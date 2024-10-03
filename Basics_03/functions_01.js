5bb684cadf29191025747e0b64d2daaa719e9000
rana-shekhar <shekharrana1011@gmail.com>
2024-10-03 19:26:34 +0530

    let's start functions

---
+++ Basics_03/functions_01.js
@@ -0,0 +1,24 @@
+function sayMyName (){
+    console.log("s");
+    console.log("h");
+    console.log("e");
+    console.log("k");
+    console.log("h");
+    console.log("a");
+    console.log("r");
+    
+}
+
+// sayMyName()
+
+function addTwoNumber(number1, number2){
+
+    let result = number1 + number2;
+    return result
+
+}
+
+const result = addTwoNumber(4,6);
+
+console.log("Result:", result);
+
