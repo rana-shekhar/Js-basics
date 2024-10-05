459883191a4683d8c7f1b2e07a60953a1b10a7f5
rana-shekhar <shekharrana1011@gmail.com>
2024-10-05 19:40:51 +0530

    little more about functions

--- Basics_03/functions_01.js
+++ Basics_03/functions_01.js
@@ -1,4 +1,4 @@
-function sayMyName (){
+function sayMyName() {
     console.log("s");
     console.log("h");
     console.log("e");
@@ -6,19 +6,65 @@ function sayMyName (){
     console.log("h");
     console.log("a");
     console.log("r");
-    
+
 }
 
 // sayMyName()
 
-function addTwoNumber(number1, number2){
+function addTwoNumber(number1, number2) {
 
-    let result = number1 + number2;
-    return result
+    // let result = number1 + number2;
+    // return result
+    return number1 + number2;
+}
+
+const result = addTwoNumber(4, 6);
+
+// console.log("Result:", result);
+
+function loginUserMessage(userName = "shekhu") {
+    if (!userName) {
+        console.log("pls enter username");
+        return
+    }
+    return `${userName} just logged in`
+}
+
+// console.log(loginUserMessage());
+
+
+
+function cartValue(var1, var2, ...num1){
+return (var1,num1,var2);
+}
+
+// console.log(cartValue(200,500,400,3000));
+
+
+const user = {
+    name: "shekhar",
+    price: 299,
+}
+
+function anyobject (handleObj){
+// console.log(`username is ${handleObj.name} and price is ${handleObj.price}`);
 
 }
 
-const result = addTwoNumber(4,6);
+// anyobject(user)
+anyobject({
+    name:"sam",
+    price:399
+})
 
-console.log("Result:", result);
+
+const myNewArray = [200,3000,400,5000]
+
+function anything(getArray){
+return getArray[1]
+}
+
+// console.log(anything(myNewArray));
+
+console.log(anything([200,400,300]));
 
